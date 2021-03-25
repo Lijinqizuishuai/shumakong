a5e7b62c623bf39c25cdcae390c1b09fd9257dd0
Lijinqizuishuai <49816660+Lijinqizuishuai@users.noreply.github.com>
2021-03-25 20:59:23 +0800

    配置redis和session信息

--- manager.py
+++ manager.py
@@ -1,20 +1,44 @@
-from flask import Flask
+from datetime import timedelta
+
+from flask import Flask,session
 from flask_sqlalchemy import SQLAlchemy
+from redis import StrictRedis
+from flask_session import Session
 
 app = Flask(__name__)
 
 class Config(object):
-    DEBUG=True
+    DEBUG = True
+    SECRET_KEY = "fsfssfs"
 
-    SQLAlCHEMY_DATABASE_URL="mysql+pymysql://root:123456@localhost:3306/info36"
-    SQLALCHEMY_TRACK_MODIFICATIONS =False
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1/info36"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = StrictRedis(host = REDIS_HOST,port= REDIS_PORT)
+    SESSION_USE_SIGNER = True
+    PERMANENT_SESSION_LIFETIME = timedelta(seconds=5)#设置session有效期
+
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT, decode_responses=True)
+
+Session(app)
+
 @app.route('/')
 def hello_world():
+    redis_store.set("name","laowang")
+    print(redis_store.get("name"))
+
+    session["name"] = "zhangsan"
+    print(session.get("name"))
     return "helloworld"
 
-if __name__=='__main__':
+if __name__ == '__main__':
+
     app.run()
