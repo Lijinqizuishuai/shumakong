3545257e8136af89331559d5d3ef28ef5d34d9ad
Lijinqizuishuai <49816660+Lijinqizuishuai@users.noreply.github.com>
2021-03-25 22:09:30 +0800

    初始化信息抽取

---
+++ info/__init__.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+from redis import StrictRedis
+from flask_session import Session
+from flask_wtf.csrf import CSRFProtect
+from config import config_dict
+from flask import Flask
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    config = config_dict.get(config_name)
+
+
+    app.config.from_object(config)
+
+    db = SQLAlchemy(app)
+
+    redis_store = StrictRedis(host=config.REDIS_HOST, port=config.REDIS_PORT, decode_responses=True)
+
+    Session(app)
+
+    CSRFProtect(app)
+    return app
--- manager.py
+++ manager.py
@@ -1,33 +1,16 @@
-from datetime import timedelta
-
-from flask import Flask,session
-from flask_sqlalchemy import SQLAlchemy
-from redis import StrictRedis
-from flask_session import Session
-from flask_wtf.csrf import CSRFProtect
-from  config import  Config
-
-app = Flask(__name__)
 
 
+from info import create_app
 
-app.config.from_object(Config)
+app = create_app("develop")
 
-db = SQLAlchemy(app)
-
-redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT, decode_responses=True)
-
-Session(app)
-
-CSRFProtect(app)
-
-@app.route('/')
+@app.route('/',methods=["GET","POST"])
 def hello_world():
-    redis_store.set("name","laowang")
-    print(redis_store.get("name"))
-
-    session["name"] = "zhangsan"
-    print(session.get("name"))
+    # redis_store.set("name","laowang")
+    # print(redis_store.get("name"))
+    #
+    # session["name"] = "zhangsan"
+    # print(session.get("name"))
     return "helloworld"
 
 if __name__ == '__main__':
